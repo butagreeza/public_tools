d55f20a325f91c7919807e303edef201aadac9d8
Thanos <francisbnagura@gmail.com>
2022-07-02 10:33:00 +0000

    Added Unused Code

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Contact from '../pages/contact/Contact';
 import Home from '../pages/home/Home';
 
 export default function MainRoute() {
